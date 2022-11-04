f3812233190f85028d9170e3b5394b9877b83473
longhuynh <huynhvohoanglong2001@gmail.com>
2022-11-04 10:07:07 +0700

    Up Tuan 9

---
+++ Tuan9/animation/components/Home.js
@@ -0,0 +1,30 @@
+import { Button, View} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function Home(props) {
+
+    const navigation = useNavigation();
+
+    const hanldPressBai1 = () => {
+        navigation.navigate("Bai1");
+    };
+    const hanldPressBai2 = () => {
+        navigation.navigate("Bai2");
+    };
+    const hanldPressBai3 = () => {
+        navigation.navigate("Bai3");
+    };
+    const hanldPressBai4 = () => {
+        navigation.navigate("Bai4");
+    };
+
+    return (
+        <View>
+            <Button title="Bai1" onPress={hanldPressBai1}/>
+            <Button title="Bai2" onPress={hanldPressBai2}/>
+            <Button title="Bai3" onPress={hanldPressBai3}/>
+        </View>
+    );
+  
+
+}
